931a60ca154768a436754e836c0269fb51e98bc5
bluegundam <45892504+bluegundam@users.noreply.github.com>
2021-03-03 23:35:56 +0900

    네비게이션 추가, 페이징 일부 적용

--- pybo/views.py
+++ pybo/views.py
@@ -3,15 +3,26 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     """
     pybo 목록 출력
     """
+    # 입력인자
+    page = request.GET.get('page', '1')
+
+    #조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list':question_list}
+
+    # 페이징처리
+    paginator = Paginator(question_list, 10) #페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list' : page_obj}
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다")
+
 
     return render(request, 'pybo/question_list.html', context)
 
